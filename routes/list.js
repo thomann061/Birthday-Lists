3d34e12d3af04c7c14273e0addb1751612eb4e46
thomann061 <thomann061@gmail.com>
2016-11-16 20:59:55 -0700

    Added minimal error handling

--- routes/list.js
+++ routes/list.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+const listSchema = require('../schemas/lists');
 
 //function to handle page requests
 const renderPage = (route, page) => {
@@ -12,25 +14,26 @@ const renderPage = (route, page) => {
 renderPage('/', 'index');
 renderPage('/index', 'index');
 
-const mongoose = require('mongoose');
-const listSchema = require('../schemas/lists');
-
 /**
  * API For a List
  */
 
+function response(message) {
+	return {
+		"message": message
+	}
+}
+
 /* POST or save a List */
 router.post('/api/save', (req, res) => {
+	const listJSON = req.body;
 	const List = mongoose.model('List', listSchema);
-	const newList = new List({
-		ListName: 'Birthday List #1',
-		ListId: 1,
-		ListItems: [ { ItemName: 'Transformers Toy' },
-					{ ItemName: 'Cool Robot' } ]
-	});
+	const newList = new List(listJSON);
 	newList.save((err, data) => {
-		if(err) console.log(err);
-		else console.log('Saved: ', data);
+		if(err)
+			res.json(response("error"));
+		else
+			res.json(response("success"));
 	});
 });
 
