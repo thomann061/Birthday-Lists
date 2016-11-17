76579aac619703520ddbb7d581cf7bd621bd2853
thomann061 <thomann061@gmail.com>
2016-11-16 20:00:30 -0700

    Moved api to list.js

--- routes/list.js
+++ routes/list.js
@@ -12,4 +12,26 @@ const renderPage = (route, page) => {
 renderPage('/', 'index');
 renderPage('/index', 'index');
 
+const mongoose = require('mongoose');
+const listSchema = require('../schemas/lists');
+
+/**
+ * API For a List
+ */
+
+/* POST or save a List */
+router.post('/api/save', (req, res) => {
+	const List = mongoose.model('List', listSchema);
+	const newList = new List({
+		ListName: 'Birthday List #1',
+		ListId: 1,
+		ListItems: [ { ItemName: 'Transformers Toy' },
+					{ ItemName: 'Cool Robot' } ]
+	});
+	newList.save((err, data) => {
+		if(err) console.log(err);
+		else console.log('Saved: ', data);
+	});
+});
+
 module.exports = router;
