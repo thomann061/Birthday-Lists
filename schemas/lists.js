09a63b340a531b0ea40884708267af92b11af165
thomann061 <thomann061@gmail.com>
2016-11-16 20:56:29 -0700

    Update to simpler schema

--- schemas/lists.js
+++ schemas/lists.js
@@ -3,8 +3,7 @@ const schema = mongoose.Schema;
 
 const list = schema({
 	ListName: { type: String, required: true, minlength: 3, maxlength: 100 },
-	ListId: { type: Number, required: true, match: /^\d+$/ },
-	ListItems: [ { ItemName: { type: String, required: true, minlength: 3, maxlength: 50 } } ]
+	ListItems: [ String ]
 });
 
 module.exports = list;
