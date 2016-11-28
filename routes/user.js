cc382760fa56053e0a5e50c853a4162ebc13537a
thomann061 <thomann061@gmail.com>
2016-11-28 10:42:21 -0700

    Add api for user

--- routes/user.js
+++ routes/user.js
@@ -13,4 +13,51 @@ renderPage('/register', 'user/register', 'Register');
 renderPage('/edit*', 'user/edit', 'Edit User');
 renderPage('/login', 'user/login', 'Login');
 
+/**
+ * API For Logging a User in
+ */
+
+function response(result, data, message) {
+	return {
+		'result': result,
+		'data': data,
+		'message': message
+	};
+}
+
+/* POST or create a User */
+router.post('/api/', (req, res) => {
+	const listJSON = req.body;
+	const List = mongoose.model('List', listSchema);
+	const newList = new List(listJSON);
+	newList.save((err, data) => {
+		if(err)
+			res.json(response('error', '', 'User was not saved.'));
+		else
+			res.json(response('success', '', 'User was saved.'));
+	});
+});
+
+/* GET a User */
+router.get('/api/:id', (req, res) => {
+	const List = mongoose.model('List', listSchema);
+	List.find({ ID: req.params.id }, (err, data) => {
+		if(err)
+			res.json(response('error', '', 'Could not get the user by that id.'));
+		else
+			res.json(response('success', data, ''));
+	});
+});
+
+/* Edit a User */
+router.put('/api/:id', (req, res) => {
+	const List = mongoose.model('List', listSchema);
+	List.findOneAndUpdate({ ID: req.params.id }, req.body, (err, data) => {
+		if(err)
+			res.json(response('error', '', 'Could not update the user.'));
+		else
+			res.json(response('success', '', 'User was updated.'));
+	});
+});
+
 module.exports = router;
